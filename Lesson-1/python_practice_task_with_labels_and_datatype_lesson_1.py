b2110b4a134bf5150ac2e67bdb51b5f299154354
BilalKhanRongotai <101033811+BilalKhanRongotai@users.noreply.github.com>
2022-08-23 09:51:52 +1200

    Add files via upload

---
+++ Lesson-1/python_practice_task_with_labels_and_datatype_lesson_1.py
@@ -0,0 +1,25 @@
+# M Bilal Khan
+#Practice task
+
+#Variable declaration
+name = "Bilal"
+height = 1.84
+address1 = "170 Coutts Street, Kibirnie, Wellington, New Zealand"
+phone = "027027027"
+hasAllergy = False
+
+
+#Datatype
+print(type(name))
+print(type(height))
+print(type(address1))
+print(type(phone))
+print(type(hasAllergy))
+
+#Data Presentation
+
+print("My name is",(name))
+print(("My height is"),(height),"metres")
+print(("Rongotai College is located on"),address1)
+print(("The phone number is "),phone)
+print(("Does"),(name),("have an allergy ="),(hasAllergy))
