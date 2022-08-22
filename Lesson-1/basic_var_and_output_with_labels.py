fa360fb1fe7cc85678c5384b3815597d5d49d9da
BilalKhanRongotai <101033811+BilalKhanRongotai@users.noreply.github.com>
2022-08-23 09:43:56 +1200

    Add files via upload

---
+++ Lesson-1/basic_var_and_output_with_labels.py
@@ -0,0 +1,10 @@
+name = "Elephant"
+numLegs = 4
+height = 4.5 
+hasTail= True
+
+
+print('The animal:',name)
+print('This number of legs:',numLegs)
+print('Height in metres:',height)
+print(hasTail)
---
+++ Lesson-1/python_practice_task_lesson_1.py
@@ -0,0 +1,11 @@
+# M Bilal Khan
+#Practice task
+
+#Variable declaration
+name = "Bilal"
+height = 1.84
+address1 = "170 Coutts Street, Kibirnie, Wellington, New Zealand"
+phone = "027027027"
+hasAllergy = False
+
+
