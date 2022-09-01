ba5e1555eaa7cbaa4745e13cb497bf6c347007bb
BilalKhanRongotai <101033811+BilalKhanRongotai@users.noreply.github.com>
2022-09-01 12:26:11 +1200

    Add files via upload

---
+++ Lesson-1/input_user.py
@@ -0,0 +1,18 @@
+# M Bilal Khan
+# Input - various types by user
+
+# input() function
+
+# Taking input from the user 
+#string = input()
+# Output
+#print()
+
+#name = input("Enter your name:")
+#print("Hello",name)
+
+#Taking input from the user as integer
+#num = int(input("Enter a number:"))
+#add = num + 1
+# Output
+#print(add)
