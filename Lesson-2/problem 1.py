7cfc16eef263fec3c5f87bdcabcf441226442cc5
BilalKhanRongotai <101033811+BilalKhanRongotai@users.noreply.github.com>
2022-09-08 12:03:16 +1200

    Add files via upload

---
+++ Lesson-2/problem 1.py
@@ -0,0 +1,11 @@
+# Bilal Khan
+# Calculation of variables. framing of labels
+
+# Problem 01
+# This program is designed to track and total goals scored by the Rongotai 3rd XI for each game played
+
+match1_ST_Pats = 7
+match2_Well_College = 5
+match3_Well_High = 10
+Team_Total = match1_ST_Pats + match2_Well_College + match3_Well_High
+print ("Rongotai College scored", Team_Total)
---
+++ Lesson-2/problem 2.py
@@ -0,0 +1,13 @@
+# Bilal Khan
+# representing number data types at string
+
+# Problem 02
+# This program is designed to track the value remaining on a gift card as it is used.
+
+Card_Balance = 50.00
+Purchace_01 = 35.00
+print ("Your Gift Card Currently Has $" + str (Card_Balance))
+
+Card_Balance = Card_Balance - Purchace_01
+print ("You Purchased an item for $" + str (Purchace_01))
+print ("Your New Balance is $" + str (Card_Balance))
