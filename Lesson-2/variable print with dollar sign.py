b70ccf42826b4723c88713fc22cf570b652d583a
BilalKhanRongotai <101033811+BilalKhanRongotai@users.noreply.github.com>
2022-09-08 12:24:54 +1200

    Add files via upload

---
+++ Lesson-2/variable print with dollar sign.py
@@ -0,0 +1,15 @@
+# M Bilal Khan
+
+
+dollars = 5
+print("$", dollars, ".00")
+
+
+print("$", end = '')
+print(dollars, end='')
+print(".00")
+
+winner = "Water Boy"
+print("First place", winner) # note the blank space between ''
+print ("First place", end = ' ')# note the blank space between ''
+print (winner)
